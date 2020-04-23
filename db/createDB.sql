785dcafffc6c3da7a85cb6e52adb0efcf98470fd
Thierry Bitz <thierry.bitz@accenture.com>
2020-04-23 18:16:52 +0200

    DB Updated

--- db/createDB.sql
+++ db/createDB.sql
@@ -1,52 +1,91 @@
-drop database if exists mygames;
-create database mygames;
-use mygames;
+drop database if exists furious_games;
+CREATE DATABASE furious_games;
+use furious_games;
 
-create table games (
-    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title varchar(100),
+create table publishers (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100)
+);
+
+CREATE TABLE games (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100),
     year_published YEAR,
-    price decimal(5,2),
-    rated varchar(10),
-    viewed int
+    suggested_price DECIMAL(5,2),
+    rated VARCHAR(10),
+    viewed INT,
+    publisher_id INT NOT NULL,
+    FOREIGN KEY (publisher_id) REFERENCES publishers(id) ON DELETE CASCADE
 );
 
-create table genres (
-    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title varchar(100)
+create table platforms (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100)
 );
 
-create table games_genres (
-    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    game_id int NOT NULL,
-    genre_id int NOT NULL,
+create table games_platforms (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    game_id INT NOT NULL,
+    platform_id INT NOT NULL,
     FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE,
-    FOREIGN KEY (genre_id) REFERENCES genres(id) ON DELETE CASCADE
+    FOREIGN KEY (platform_id) REFERENCES platforms(id) ON DELETE CASCADE
 );
 
 create table developers (
-	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title varchar(100),
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100), 
     year_founded YEAR
 );
 
 create table games_developers (
-    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    game_id int NOT NULL,
-    developer_id int NOT NULL,
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    game_id INT NOT NULL,
+    developer_id INT NOT NULL,
     FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE,
     FOREIGN KEY (developer_id) REFERENCES developers(id) ON DELETE CASCADE
 );
 
-create table platforms (
-    id int not null auto_increment PRIMARY KEY,
-    title varchar(100)
+create table genres (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100)
 );
 
-create table games_platforms (
-    id int not null auto_increment PRIMARY KEY,
-    game_id int NOT NULL,
-    platform_id int NOT NULL,
+create table games_genres (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    game_id INT NOT NULL,
+    genre_id INT NOT NULL,
     FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE,
-    FOREIGN KEY (platform_id) REFERENCES platforms(id) ON DELETE CASCADE
+    FOREIGN KEY (genre_id) REFERENCES genres(id) ON DELETE CASCADE
+);
+
+create table reviewers (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100), 
+    email VARCHAR(100)
+);
+
+create table reviews (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    game_id INT NOT NULL,
+    reviewer_id INT NOT NULL,
+    review VARCHAR(140),
+    score DECIMAL(2,1),
+	FOREIGN KEY (reviewer_id) REFERENCES reviewers(id) ON DELETE CASCADE,
+    FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE
+);
+
+create table games_reviewers (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    game_id INT NOT NULL,
+    reviewer_id INT NOT NULL,
+    FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE,
+    FOREIGN KEY (reviewer_id) REFERENCES reviewers(id) ON DELETE CASCADE
+);
+
+create table games_reviews (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    game_id INT NOT NULL,
+    review_id INT NOT NULL,
+    FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE,
+    FOREIGN KEY (review_id) REFERENCES reviews(id) ON DELETE CASCADE
 );
--- db/createData.sql
+++ db/createData.sql
@@ -1,6 +1,13 @@
-use mygames;
-
-START TRANSACTION;
+use furious_games;
+SET FOREIGN_KEY_CHECKS = 0;
+truncate developers;
+truncate genres;
+truncate games;
+truncate platforms;
+truncate publishers;
+truncate reviewers;
+truncate reviews;
+SET FOREIGN_KEY_CHECKS = 1;
 
 INSERT into developers
 (id, title,year_founded)
@@ -33,24 +40,33 @@ values
 ("Simulation"),
 ("Fighting Games");
 
-insert into games
-(id, title,year_published,price,rated)
+insert into publishers
+(id, title)
 values
-(1,"TemTem",2020,25.00,"FSK0"),
-(2,"Halo",2001,10.00,"FSK12"),
-(3,"Amnesia: The Dark Descent", 2010, 20.00, "FSK18"),
-(4,"The Last of Us", 2013, 60.00, "FSK18"),
-(5,"Dark Souls", 2011, 30.00, "FSK16"),
-(6,"World of Warcraft",2007,25.00,"FSK12"),
-(7,"Warcraft 3",2002,30.00,"FSK0"),
-(8,"The Sims 4", 2014, 50.00, "FSK6"),
-(9,"The Elder Scrolls V: Skyrim", 2011, 49.99, "FSK12"),
-(10,"Uncharted: Drakes Schicksal", 2007, 60.00, "FSK16"),
-(11,"Super Mario Bros WII Edition",2008,59.99,"FSK0"),
-(12,"Madden 20", 2020, 59.99, "FSK0"),
-(13,"Tekken 7", 2015,59.99,"FSK12"),
-(14,"Pac-Man", 1980, 19.99, "FSK0"),
-(15,"GTA San Andreas",2002,59.99,"FSK18");
+(1, 'Blizzard'),
+(2, 'Namco'),
+(3, 'Bethesda'),
+(5, 'cremagames'),
+(4, 'Unknown');
+
+insert into games
+(id, title,year_published,suggested_price,rated, publisher_id)
+values
+(1,"TemTem",2020,25.00,"FSK0", 5),
+(2,"Halo",2001,10.00,"FSK12", 4),
+(3,"Amnesia: The Dark Descent", 2010, 20.00, "FSK18", 3),
+(4,"The Last of Us", 2013, 60.00, "FSK18", 4),
+(5,"Dark Souls", 2011, 30.00, "FSK16", 4),
+(6,"World of Warcraft",2007,25.00,"FSK12", 1),
+(7,"Warcraft 3",2002,30.00,"FSK0", 1),
+(8,"The Sims 4", 2014, 50.00, "FSK6", 4),
+(9,"The Elder Scrolls V: Skyrim", 2011, 49.99, "FSK12", 4),
+(10,"Uncharted: Drakes Schicksal", 2007, 60.00, "FSK16", 4),
+(11,"Super Mario Bros WII Edition",2008,59.99,"FSK0", 4),
+(12,"Madden 20", 2020, 59.99, "FSK0", 4),
+(13,"Tekken 7", 2015,59.99,"FSK12", 2),
+(14,"Pac-Man", 1980, 19.99, "FSK0", 2),
+(15,"GTA San Andreas",2002,59.99,"FSK18", 4);
 
 insert into platforms
 (title)
@@ -61,14 +77,27 @@ values
 
 insert into games_developers (game_id,developer_id)
 values (1,3),(13,10);
-insert into games_genres (game_id,genre_id)
-values (1,7),(13,2),(11,7);
-insert into games_platforms (game_id,platform_id)
+
+insert into reviewers (id, title, email) values
+(1, "thierry", "test@gmail.com"),
+(2, "steffi", "steffi@gmail.com"),
+(3, "allen", "allen@gmx.ch");
+
+insert into reviews (id,game_id,reviewer_id, review, score)
+values
+(1, 5, 1, "Geiles Game", 3.2),
+(2, 6, 1, "Super Game", 4.1),
+(3, 7, 2, "Game des Jahres", 2.1),
+(4, 8, 3, "BS", 5.0),
+(5, 9, 3, "Naja", 1.7);
+
+insert into games_reviewers (game_id, reviewer_id)
+values (1,1),(2,2),(3,3),(5,3),(6,3);
+
+insert into games_platforms (game_id, platform_id)
 values (1,6),(13,4),(13,6),(13,13),(11,14);
 
-insert into games_developers (game_id, developer_id) values (9,7);
-insert into games_genres (game_id, genre_id) values (9,9);
-insert into games_platforms (game_id, platform_id) values (9,4),(9,6),(9,12),
-(9,13);
+insert into games_genres (game_id, genre_id)
+values (1,7),(13,2),(11,7);
 
-COMMIT;
+
